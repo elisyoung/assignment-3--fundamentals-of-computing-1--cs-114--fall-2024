05ff0494181b3763f59fe57310fc3566cfa55b95
elisyoung <eyoung@hartford.edu>
2024-11-06 15:47:48 -0500

    took user input for matrix size

--- RunMatrix.java
+++ RunMatrix.java
@@ -2,5 +2,22 @@ import java.util.Scanner;
 
 public class RunMatrix {
   public static void main(String[] args) {
+    int matrixSize;
+
+    //get a value from the user
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter a positive integer: ");
+    matrixSize = input.nextInt();
+    System.out.println();
+
+    while(matrixSize < 1){
+      System.out.print("This value is not positive, enter a positive integer: ");
+      matrixSize = input.nextInt();
+    }
+
+    input.close();
+
+
+
   }
 }
